71849ca031b362ec5b66a840a849ed561fc98946
wilsontom <wilsontom89@gmail.com>
2019-10-07 21:15:07 +0100

    configure ENTRYPOINT using expect

--- aberconnect.sh
+++ aberconnect.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
+#!/usr/bin/expect -f
 
-./openconnect --protocol=gp pa-vpn.aber.ac.uk  
+set timeout -1
+
+set username [lindex $argv 0]
+set password [lindex $argv 1]
+
+spawn ./openconnect --protocol=gp pa-vpn.aber.ac.uk --user=$username
+
+expect "*OTP:*"
+
+send -- "$password\r"
+
+expect eof
+
