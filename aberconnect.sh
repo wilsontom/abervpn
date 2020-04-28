c93b7cc7a9c41453eb152f27bd26b4ae96295eef
wilsontom <wilsontom89@gmail.com>
2020-04-28 12:33:08 +0100

    configue parameters

--- aberconnect.sh
+++ aberconnect.sh
@@ -4,12 +4,15 @@ set timeout -1
 
 set username [lindex $argv 0]
 set password [lindex $argv 1]
+set OTP [lindex $argv 2]
 
 spawn ./openconnect --protocol=gp pa-vpn.aber.ac.uk --user=$username
 
-expect "*OTP:*"
-
+expect "Password:*"
 send -- "$password\r"
+
+expect "*OTP:*"
+send -- $OTP\r"
 
 expect eof
 
